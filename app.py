d794c237ac51ec4ef2b6f9b29cfebe971017acfc
sijokg <cijo_k_george@yahoo.co.in>
2019-10-31 01:50:50 +0530

    db importing problem solved

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.secret_key = 'sijo'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-  db.create_all()
+
 
 jwt = JWT(app, authenticate, identity)  #This will give /auth end point
 
